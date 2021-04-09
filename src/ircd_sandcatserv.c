80dd20dca01f0cbbd82b7d310af33b7bb4a27520
jesopo <github@lolnerd.net>
2021-04-09 09:53:50 +0000

    better catserv

--- src/ircd_sandcatserv.c
+++ src/ircd_sandcatserv.c
@@ -7,54 +7,54 @@
 
 #include "atheme-compat.h"
 
-static service_t *catserv = NULL;
+static service_t *sandcatserv = NULL;
 
 static void
-catserv_cmd_meow(sourceinfo_t *si, int parc, char *parv[])
+sandcatserv_cmd_meow(sourceinfo_t *si, int parc, char *parv[])
 {
 	command_success_nodata(si, "Meow!");
 }
 
 static void
-catserv_cmd_help(sourceinfo_t *si, int parc, char *parv[])
+sandcatserv_cmd_help(sourceinfo_t *si, int parc, char *parv[])
 {
 	command_help(si, si->service->commands);
 }
 
-static command_t catserv_meow = {
+static command_t sandcatserv_meow = {
 	.name           = "MEOW",
 	.desc           = N_("Makes the cute little kitty-cat meow!"),
 	.access         = AC_NONE,
 	.maxparc        = 0,
-	.cmd            = &catserv_cmd_meow,
+	.cmd            = &sandcatserv_cmd_meow,
 	.help           = { .path = "" },
 };
 
-static command_t catserv_help = {
+static command_t sandcatserv_help = {
 	.name           = "HELP",
 	.desc           = N_("Displays contextual help information."),
 	.access         = AC_NONE,
 	.maxparc        = 1,
-	.cmd            = &catserv_cmd_help,
+	.cmd            = &sandcatserv_cmd_help,
 	.help           = { .path = "help" },
 };
 
 static void
 mod_init(module_t *const restrict m)
 {
-	catserv = service_add("catserv", NULL);
+	sandcatserv = service_add("sandcatserv", NULL);
 
-	service_bind_command(catserv, &catserv_meow);
-	service_bind_command(catserv, &catserv_help);
+	service_bind_command(sandcatserv, &sandcatserv_meow);
+	service_bind_command(sandcatserv, &sandcatserv_help);
 }
 
 static void
 mod_deinit(const module_unload_intent_t intent)
 {
-	service_unbind_command(catserv, &catserv_meow);
-	service_unbind_command(catserv, &catserv_help);
+	service_unbind_command(sandcatserv, &sandcatserv_meow);
+	service_unbind_command(sandcatserv, &sandcatserv_help);
 
-	service_delete(catserv);
+	service_delete(sandcatserv);
 }
 
-SIMPLE_DECLARE_MODULE_V1("contrib/ircd_catserv", MODULE_UNLOAD_CAPABILITY_OK)
+SIMPLE_DECLARE_MODULE_V1("contrib/ircd_sandcatserv", MODULE_UNLOAD_CAPABILITY_OK)
